5f69847f2ebc226e48458dcb82382ab3b97e8f04
Muadil87 <elbahlouli.adil@etu.uae.ac.ma>
2026-03-28 19:45:35 +0100

    Fix timetable, add student filtering, fix proposal approve/reject

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.timetable_list, name='timetable_list'),
-    path('add/', views.add_timetable, name='add_timetable'),
+    path('add-timetable/', views.add_timetable, name='add_timetable'),
     path('visual-tool/', views.visual_timetabling_embed, name='visual_tool'),
     path('export-json/', views.export_timetable_json, name='export_json'),
+
+    # PROPOSALS
+    path('add-proposal/', views.add_proposal, name='add_proposal'),
+    path('proposals/', views.proposal_list, name='proposal_list'),
+    path('proposal/<int:proposal_id>/approve/', views.approve_proposal, name='approve_proposal'),
+     path('proposal/<int:proposal_id>/reject/', views.reject_proposal, name='reject_proposal'),
 ]
